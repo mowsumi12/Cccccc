3a775eb76e424fd8285aac66ba4d7e66c26e2422
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-12-25 10:37:46 +0600

    Update baby.js

--- TINA/commands/baby.js
+++ TINA/commands/baby.js
@@ -21,15 +21,12 @@ module.exports.run = async function ({ api, event, args, Users }) {
     const link = `${await baseApiUrl()}/baby`;
     const dipto = args.join(" ").toLowerCase();
     const uid = event.senderID;
-    const name = await Users.getNameUser(event.senderID);
+const name = await Users.getNameUser(event.senderID);
 
     if (!args[0]) {
       const ran = ["Bolo baby", "hum", "type help baby", "type !baby hi"];
       const r = ran[Math.floor(Math.random() * ran.length)];
-      try {
-        if (!dipto) {
-            return api.sendMessage(`${name},\n\n${ran}\n\n`, event.threadID, event.messageID);
-        }
+      return api.sendMessage(`${namee}, ${r}`, event.threadID, event.messageID);
 
     if (args[0] === 'remove') {
       const fina = dipto.replace("remove ", "");
