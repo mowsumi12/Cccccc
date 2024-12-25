fea39a6bc9fbdc91eb8d34ed9662bc28a4684870
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-12-25 22:38:58 +0600

    Update baby.js

--- TINA/commands/baby.js
+++ TINA/commands/baby.js
@@ -150,7 +150,7 @@ try{
 }};
 
    
-module.exports.handleEvent = async function ({ api, event, Users }) {
+module.exports.handleEvent = async function ({ api, event, Users, events }) {
 try{
     const body = event.body ? event.body.toLowerCase() : ""
     if(body.startsWith("sumi") || body.startsWith("riya") || body.startsWith("সুমি")){
