94609dd23f20f993a705058f01892351ff757faf
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-15 22:19:31 +0600

    Update bing.js

--- TINA/commands/bing.js
+++ TINA/commands/bing.js
@@ -3,44 +3,49 @@ const fs = require('fs-extra');
 const path = require('path');
 
 module.exports.config = {
-    name: "bing",
-    version: "1.0",
-    credits: "Aadi Gupta",
-    hasPermssion: 2,
-    description: "Generate images by Dalle-3 AI",
-    commandCategory: "download",
-    usages: "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written Aadi and 69 on the back of his Dress , 4k]",
-    cooldowns: 5
-  };
+  name: "Bing",
+  version: "10.2",
+  hasPermssion: 0,
+  credits: "Hazeyy",
+  commandCategory: "",
+  cooldowns: 30,
+  hasPrefix: false,
+};
 
 module.exports.run = async function ({ api, event, args }) {
-  const prompt = event.messageReply?.body.split("dalle")[1] ||  args.join(" ");
-  if (!prompt) {
-   return api.sendMessage("❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written Aadi and 69 on the back of his Dress , 4k",event.threadID,event.messageID);
+
+  if (args.length === 0) {
+    api.sendMessage("🐱 𝙷𝚎𝚕𝚕𝚘 𝚝𝚘 𝚞𝚜𝚎 𝙱𝚒𝚗𝚐 𝙳𝚊𝚕𝚕𝚎\n\n 𝙿𝚕𝚎𝚊𝚜𝚎 𝚞𝚜𝚎: 𝙱𝚒𝚗𝚐 [ 𝚙𝚛𝚘𝚖𝚙𝚝 ]", event.threadID, event.messageID);
+    return;
   }
+
     try {
-      const w = await api.sendMessage("Waiting few seconds baby < 😽", event.threadID);
-  
-const response = await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/dalle?prompt=${prompt}&key=dipto008&cookies=1xx-CWi5lgL8CItH5p0YiIlS9N25oFEii5TAU7XZCFVH5qNkZVnsPgw2Q8-ICPcG-g755akN5DObO7PpgBJ2ILhvk8qDDc4J1IUAdxDwriZKO2MdaLOIX_VTMaooFIAiUzHMwAPTkOM7zwsym74buHdvPgpiUhTvAqny167B9ei2_0TtgH74qT3jyQEhOQePW6MHr_SHGSP3JuHAqnpybnx-4XvwQwRRWcASQlz6nYpo`)
-      const data = response.data.imgUrls;
-      if (!data || data.length === 0) {
-        api.sendMessage("Empty response or no images generated.",event.threadID,event.messageID);
-      }
-      const diptoo = [];
-      for (let i = 0; i < data.length; i++) {
-        const imgUrl = data[i];
-        const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
-        const imgPath = path.join(__dirname, 'dalle', `${i + 1}.jpg`);
-        await fs.outputFile(imgPath, imgResponse.data);
-        diptoo.push(fs.createReadStream(imgPath));
-      }
-      await api.unsendMessage(w.messageID);
-      await api.sendMessage({
-  body: `✅ |Your pic Generated <😘`,
-        attachment: diptoo
-      },event.threadID, event.messageID);
+        const prompt = args.join(" ");
+
+        api.sendMessage("🕟 | 𝙱𝚒𝚗𝚐 𝙶𝚎𝚗𝚎𝚛𝚊𝚝𝚒𝚗𝚐 𝚙𝚛𝚘𝚖𝚙𝚝, 𝙿𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝...", event.threadID, event.messageID);
+
+        const url = `https://hazee-bing-7ad593c7e1e1.herokuapp.com/bing/dalle?query=${encodeURIComponent(prompt)}`;
+
+        const response = await axios.get(url);
+        const data = response.data;
+
+        const imgData = [];
+
+        for (let i = 0; i < data.images.length; i++) {
+            const imgUrl = data.images[i].url;
+            const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+            const imgPath = path.join(__dirname, 'cache', `${i + 1}.jpg`);
+            await fs.outputFile(imgPath, imgResponse.data);
+            imgData.push(fs.createReadStream(imgPath));
+        }
+
+        await api.sendMessage({
+            body: `🤖 𝐁𝐢𝐧𝐠 ( 𝐀𝐈 )\n\n𝙷𝚎𝚛𝚎'𝚜 𝚢𝚘𝚞𝚛 𝚙𝚛𝚘𝚖𝚙𝚝:`,
+            attachment: imgData
+        }, event.threadID, event.messageID);
+
     } catch (error) {
-      console.error(error);
-      await api.sendMessage(`Generation failed!\nError: ${error.message}`,event.threadID, event.messageID);
+        console.error(error);
+        await api.sendMessage(`🤖 𝙱𝚒𝚗𝚐 𝙶𝚎𝚗𝚎𝚛𝚊𝚝𝚒𝚘𝚗 𝚏𝚊𝚒𝚕𝚎𝚍!\n\n𝙴𝚛𝚛𝚘𝚛: ${error.message}`, event.threadID);
     }
-  };
+};
